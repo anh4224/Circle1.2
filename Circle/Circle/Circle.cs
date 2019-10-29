f55934a7d4ec2071c8d16810db229a40d6f5b2a7
quanganh <bantia447@gmail.com>
2019-10-29 17:52:53 +0700

    Circle

---
+++ Circle/Circle/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    public class Circle
+    {
+        private double Radius = 1.0;
+
+        public Circle()
+        {
+
+        }
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public void setRadius(double Radius)
+        {
+            this.Radius = Radius;
+
+        }
+        public double getArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+        public double getCircumference()
+        {
+            return Radius;
+        }
+        public double getRadius()
+        {
+            return this.Radius;
+        }
+        public override string ToString()
+        {
+            return String.Format("Circle [ Radius = {0}}", Radius);
+        }
+    }
+}
---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            Console.WriteLine("The circle has radius of {0} and area of {1} and Circumference {2}",
+                c1.getRadius(), c1.getArea(), c1.getCircumference());
+            Circle c2 = new Circle(2);
+            Console.WriteLine("The circle has radius of {0} and area of {1} and Circumference {2}",
+                c2.getRadius(), c2.getArea(), c2.getCircumference());
+
+        }
+    }
+}
